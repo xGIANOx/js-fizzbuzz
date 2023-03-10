2d9d886b6a4b56697f86d3471643dc68d6ed0cdc
xGIANOx <studioenricogian@gmail.com>
2023-03-10 16:58:49 +0100

    [add] layout with .col

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,15 @@
-const ulElement = document.querySelector("ul")
+const rowElement = document.querySelector(".row")
  
 
 
 
 for (let i = 1; i <= 100; i++) {
 
-    document.getElementById('ulList').style.display = 'flex'
-    document.getElementById('ulList').style.flexDirection = 'row'
-    document.getElementById('ulList').style.flexWrap = 'wrap'
-    document.getElementById('ulList').style.listStyle = 'none'
-    document.getElementById('ulList').style.padding = '1rem'
+    //document.getElementById('ulList').style.display = 'flex'
+    //document.getElementById('ulList').style.flexDirection = 'row'
+    //document.getElementById('ulList').style.flexWrap = 'wrap'
+    //document.getElementById('ulList').style.listStyle = 'none'
+    //document.getElementById('ulList').style.padding = '1rem'
     
 
 
@@ -18,23 +18,23 @@ for (let i = 1; i <= 100; i++) {
 
     if (i % 3 === 0 && i % 5 === 0) {
       let i = 'fizzbuzz'
-      const liElement = `<li class="fizzbuzz">${i}</li>`
-      ulElement.innerHTML += liElement
+      const colElement = `<div class="col fizzbuzz">${i}</div>`
+      rowElement.innerHTML += colElement
     } else if (i % 3 === 0) {
       console.log("Fizz");
       let i = 'fizz'
-      const liElement = `<li class="fizz">${i}</li>`
-      ulElement.innerHTML += liElement
+      const colElement = `<div class="col fizz">${i}</div>`
+      rowElement.innerHTML += colElement
 
     } else if (i % 5 === 0) {
       console.log("Buzz");
       let i = 'buzz'
-      const liElement = `<li class="buzz">${i}</li>`
-      ulElement.innerHTML += liElement
+      const colElement = `<div class="col buzz">${i}</div>`
+      rowElement.innerHTML += colElement
     } else {
       console.log(i);
-      const liElement = `<li class="number">${i}</li>`
-      ulElement.innerHTML += liElement
+      const colElement = `<div class="col number">${i}</div>`
+      rowElement.innerHTML += colElement
             
     }
 
